9590f4117272b72be7015507791b4fd93b643a76
Michael Ibuaku <mujiangelo17@gmail.com>
2025-04-18 01:40:47 +0300

    now?

--- app/api/payments/route.js
+++ app/api/payments/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import { getServerSession } from "next-auth/next"
 import { sql } from "@/lib/db"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { authOptions } from "@/app/utils/auth"
 import { paystack } from "@/lib/paystack"
 
 export async function POST(request) {
